07556fba50ae73f8a7769e27b966b5b860400f15
Hugo Novo Gouvea <97973663+Hugo-Novo-Gouvea@users.noreply.github.com>
2026-01-13 15:03:27 -0300

    Teste de criação de produtos

---
+++ GestaoClientes.Web/Models/Cliente.cs
@@ -0,0 +1,10 @@
+namespace GestaoClientes.Web.Models; // Note que o namespace é .Web
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public bool Ativo { get; set; }
+}
